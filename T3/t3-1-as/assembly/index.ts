0d8c75140ed489d4822499c48d68c93253157b7a
小粮仔 <169179797@qq.com>
2024-04-04 19:19:53 +0800

    [feat]: Pass T3-1 test

---
+++ T3/t3-1-as/assembly/index.ts
@@ -0,0 +1,144 @@
+// The entry file of your WebAssembly module.
+
+export function mancalaBoard(
+  flag: i32,
+  seq: Array<i32>,
+  size: i32
+): Array<i32> {
+  let board = new Array<i32>(15);
+  for (let i = 0; i < 14; i++) {
+    board[i] = 4;
+  }
+  board[6] = 0;
+  board[13] = 0;
+  let nextDirec = seq[0] / 10;
+  for (let i = 0; i < size; i++) {
+    let direct = seq[i] / 10;
+    let hole = seq[i] % 10;
+    hole = direct === 2 ? 7 - hole : hole;
+    let piece = board[(direct - 1) * 7 + hole - 1];
+    if(i === (size - 1) && (direct != nextDirec || piece === 0)){
+      if(direct == 1){
+        board[14] = 200 + 2 * board[6] - 48;
+      }else{
+        board[14] = 200 + 48 - 2 * board[13];
+      }
+      reverseSubarray(board, 7, 12);
+      return board;
+    }
+    let nextFlag = false;
+    board[(direct - 1) * 7 + hole - 1] = 0;
+    while (piece > 0) {
+      if (direct === 1) {
+        for (let j = hole + 1; j <= 6 && piece > 0; j++) {
+          board[j - 1]++;
+          piece--;
+          hole++;
+        }
+        if (piece === 0) {
+          break;
+        }
+        hole = -1;
+        board[6]++;
+        piece--;
+        if (piece === 0) {
+          nextFlag = true; //shouldNext
+          break;
+        }
+        for (let j = 6; j > 0 && piece > 0; j--) {
+          board[6 + j]++;
+          piece--;
+        }
+        if (piece === 0) {
+          break;
+        }
+        hole = 0;
+      } else if (direct === 2) {
+        for (let j = hole - 1; j > 0 && piece > 0; j--) {
+          board[6 + j]++;
+          piece--;
+          hole--;
+        }
+        if (piece === 0) {
+          break;
+        }
+        hole = -1;
+        board[13]++;
+        piece--;
+        if (piece === 0) {
+          nextFlag = true; //shouldNext
+          break;
+        }
+        for (let j = 1; j <= 6 && piece > 0; j++) {
+          board[j - 1]++;
+          piece--;
+        }
+        if (piece === 0) {
+          break;
+        }
+        hole = 7;
+      }
+    }
+    if (!nextFlag) {
+      nextDirec = 3 - direct;
+    } else {
+      nextDirec = direct;
+    }
+    if (direct === 1) {
+      if (hole > 0 && board[hole - 1] === 1 && board[6 + hole] != 0) {
+        board[6] += board[hole - 1] + board[6 + hole];
+        board[6 + hole] = 0;
+        board[hole - 1] = 0;
+      }
+    } else if (direct === 2) {
+      if (hole > 0 && board[6 + hole] === 1 && board[hole - 1] != 0) {
+        board[13] += board[hole - 1] + board[6 + hole];
+        board[6 + hole] = 0;
+        board[hole - 1] = 0;
+      }
+    }
+  }
+  let res = isFinish(board);
+  if (res == 0) {
+    board[14] = nextDirec;
+    reverseSubarray(board, 7, 12);
+    return board;
+  }
+  if (res == 1) {
+    for (let j = 7; j <= 12; j++) {
+      board[13] += board[j];
+      board[j] = 0;
+    }
+  } else {
+    for (let j = 0; j <= 5; j++) {
+      board[6] += board[j];
+      board[j] = 0;
+    }
+  }
+  board[14] = 200 + (board[6] - board[13]);
+  reverseSubarray(board, 7, 12);
+  return board;
+}
+
+export function isFinish(borad:Array<i32>): i32 {
+  let side1Empty = borad.slice(0,6).every(hole => hole === 0);
+  let side2Empty = borad.slice(7,13).every(hole => hole === 0);
+  console.log("side1 " + side1Empty.toString());
+  console.log("side2 " + side2Empty.toString());
+  if (side1Empty) return 1;
+  if (side2Empty) return 2;
+  return 0;
+}
+
+
+function reverseSubarray<T>(array: T[], start: i32, end: i32): void {
+  if (start < 0) start = 0;
+  if (end >= array.length) end = array.length - 1;
+  while (start < end) {
+    let temp: T = array[start];
+    array[start] = array[end];
+    array[end] = temp;
+    start++;
+    end--;
+  }
+}
--- T3/test.js
+++ T3/test.js
@@ -1,12 +1,13 @@
 import assert from "assert";
+import fs from 'fs';
 
 // Choose proper "import" depending on your PL.
-import { mancalaOperator as op1 } from "./t3-2-as/build/release.js";
+// import { mancalaOperator as op1 } from "./t3-2-as/build/release.js";
 // import { mancala_operator as op1 } from "./t3_2_rust/pkg/t3_2_rust.js"
 // [Write your own "import" for other PLs.]
 
 // Choose proper "import" depending on your PL.
-import { mancalaOperator as op2 } from "./t3-2-as-rival/build/release.js";
+// import { mancalaOperator as op2 } from "./t3-2-as-rival/build/release.js";
 // import { mancala_operator as op2 } from "./t3_2_rust_rival/pkg/t3_2_rust.js"
 // [Write your own "import" for other PLs.]
 
@@ -15,80 +16,155 @@ import { mancalaBoard as board } from "./t3-1-as/build/release.js";
 // import { mancala_board as board } from "./t3_1_rust/pkg/t3_1_rust.js"
 // [Write your own "import" for other PLs.]
 
-let operator, status, operation, operationSequence, boardReturn, isEnded;
-let op1Result = 0, op2Result = 0;
-let op1Time = 0, op2Time = 0, timeStamp = 0;
+function readTXTFile(file) {
+    var lines = file.split('\n');
 
-// Firstly, start from op1.
-operator = 1;
-status = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
-operation = 0;
-operationSequence = [];
-isEnded = false;
+    lines.forEach(function(line, index) {
+        if (line) {
+            // console.log('Line ' + index + ' : ' + line);
+            var obj = JSON.parse(line);
+            // console.log(obj);
+            var type = obj.type;
+            var steps = obj.steps;
 
-do {
-    if (operator == 1) {
-        timeStamp = performance.now() * 1000;
-        operation = op1(1, status);
-        op1Time += performance.now() * 1000 - timeStamp;
-        operationSequence.push(operation);
-        boardReturn = board(1, operationSequence, operationSequence.length);
-    } else {
-        timeStamp = performance.now() * 1000;
-        operation = op2(2, status);
-        op2Time += performance.now() * 1000 - timeStamp;
-        operationSequence.push(operation);
-        boardReturn = board(2, operationSequence, operationSequence.length);
-    }
-    if (boardReturn[14] == 1) {
-        operator = 1;
-        status = boardReturn.slice(0,14);
-    } else if (boardReturn[14] == 2) {
-        operator = 2;
-        status = boardReturn.slice(0,14);
-    } else {
-        isEnded = true;
-        op1Result += boardReturn[14] - 200;
-        op2Result -= boardReturn[14] - 200;
-    }
-} while (!isEnded);
+            if (type === "complete") {
+                var winner = obj.winner;
+                var winning_points = obj.winning_points;
+                var last_step = steps[steps.length - 1];
+                var res_board = board(Math.floor(last_step / 10), steps, steps.length);
 
-// Now change to start from op2.
-operator = 2;
-status = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
-operation = 0;
-operationSequence = [];
-isEnded = false;
+                console.log(res_board);
+                assert.strictEqual(res_board[14], winning_points, "The type is:" + type + " The line is: " + line);
+                
+                // if (winner === 0) {
+                //     var last_step = steps[steps.length - 1];
+                //     res_board = board(last_step / 10, steps, steps.length);
 
-do {
-    if (operator == 1) {
-        timeStamp = performance.now() * 1000;
-        operation = op1(1, status);
-        op1Time += performance.now() * 1000 - timeStamp;
-        operationSequence.push(operation);
-        boardReturn = board(1, operationSequence, operationSequence.length);
-    } else {
-        timeStamp = performance.now() * 1000;
-        operation = op2(2, status);
-        op2Time += performance.now() * 1000 - timeStamp;
-        operationSequence.push(operation);
-        boardReturn = board(2, operationSequence, operationSequence.length);
-    }
-    if (boardReturn[14] == 1) {
-        operator = 1;
-        status = boardReturn.slice(0,14);
-    } else if (boardReturn[14] == 2) {
-        operator = 2;
-        status = boardReturn.slice(0,14);
-    } else {
-        isEnded = true;
-        op1Result += boardReturn[14] - 200;
-        op2Result -= boardReturn[14] - 200;
-    }
-} while (!isEnded);
+                //     assert.strictEqual(res_board[14], winning_points, "The type is:" + type + " The line is: " + line);
+                //     // assert.strictEqual(mancalaResult(1, steps, steps.length), winning_points,  "The type is:" + type + " The file is: " + file + " The line is: " + line);
+                // } else {
+                //     assert.strictEqual(mancalaResult(1, steps, steps.length), winning_points,  "The type is:" + type + " The file is: " + file + " The line is: " + line);
+                // }
+            } 
+            else if (type === "incomplete") {
+                var next_turn = obj.next_turn;
+                var last_step = steps[steps.length - 1];
+                var res_board = board(Math.floor(last_step / 10), steps, steps.length);
+                console.log(res_board);
+                assert.strictEqual(res_board[14], next_turn + 1, "The type is:" + type + " The line is: " + line);
+                // assert.strictEqual(mancalaResult(1, steps, steps.length), first_points,  "The type is:" + type + " The file is: " + file + " The line is: " + line);
+            } 
+            else if (type === "illegal") {
+                var illegal_points = obj.illegal_points;
+                var illegal_player = obj.illegal_player;
+                var res_board = board(illegal_player + 1, steps, steps.length);
+                console.log(res_board);
+                assert.strictEqual(res_board[14], illegal_points, "The type is:" + type + " The line is: " + line);
+                // assert.strictEqual(mancalaResult(1, steps, steps.length), points, "The type is:" + type + " The file is: " + file + " The line is: " + line);
+            }
+            // assert.strictEqual(mancalaResult(1, steps, steps.length), pure_points);
+        }
+    })
+}
+
+
+fs.readFile('output_T3_0.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    readTXTFile(data);
+    // assert.strictEqual(mancalaResult(1,[13, 16, 13, 23, 12, 25, 13, 24, 12, 15, 24, 25], 12),30001);
+    // let steps = [13, 14, 22, 23, 16, 24, 11, 16, 14, 23, 12, 22, 16, 15, 25, 14, 24, 15, 13, 26, 21, 14, 24, 11, 23, 16];
+    // let steps = [13]
+    // let res_board = board(1, steps, steps.length);
+    // loop print the board in each step
+    // for (let i = 0; i < steps.length; i++) {
+    //     console.log(Math.floor(steps[i] / 10));
+    //     console.log(steps.slice(0, i + 1));
+    //     let res_board = board(Math.floor(steps[i] / 10), steps.slice(0, i + 1), i + 1);
+    //     console.log("Step: " + i.toString());
+    //     console.log(steps[i]);
+    //     console.log(res_board);
+    // }
+
+    console.log("🎉 You have passed all the tests provided.");
+
+});
+
+
+
+// let operator, status, operation, operationSequence, boardReturn, isEnded;
+// let op1Result = 0, op2Result = 0;
+// let op1Time = 0, op2Time = 0, timeStamp = 0;
+
+// // Firstly, start from op1.
+// operator = 1;
+// status = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
+// operation = 0;
+// operationSequence = [];
+// isEnded = false;
+
+// do {
+//     if (operator == 1) {
+//         timeStamp = performance.now() * 1000;
+//         operation = op1(1, status);
+//         op1Time += performance.now() * 1000 - timeStamp;
+//         operationSequence.push(operation);
+//         boardReturn = board(1, operationSequence, operationSequence.length);
+//     } else {
+//         timeStamp = performance.now() * 1000;
+//         operation = op2(2, status);
+//         op2Time += performance.now() * 1000 - timeStamp;
+//         operationSequence.push(operation);
+//         boardReturn = board(2, operationSequence, operationSequence.length);
+//     }
+//     if (boardReturn[14] == 1) {
+//         operator = 1;
+//         status = boardReturn.slice(0,14);
+//     } else if (boardReturn[14] == 2) {
+//         operator = 2;
+//         status = boardReturn.slice(0,14);
+//     } else {
+//         isEnded = true;
+//         op1Result += boardReturn[14] - 200;
+//         op2Result -= boardReturn[14] - 200;
+//     }
+// } while (!isEnded);
+
+// // Now change to start from op2.
+// operator = 2;
+// status = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
+// operation = 0;
+// operationSequence = [];
+// isEnded = false;
+
+// do {
+//     if (operator == 1) {
+//         timeStamp = performance.now() * 1000;
+//         operation = op1(1, status);
+//         op1Time += performance.now() * 1000 - timeStamp;
+//         operationSequence.push(operation);
+//         boardReturn = board(1, operationSequence, operationSequence.length);
+//     } else {
+//         timeStamp = performance.now() * 1000;
+//         operation = op2(2, status);
+//         op2Time += performance.now() * 1000 - timeStamp;
+//         operationSequence.push(operation);
+//         boardReturn = board(2, operationSequence, operationSequence.length);
+//     }
+//     if (boardReturn[14] == 1) {
+//         operator = 1;
+//         status = boardReturn.slice(0,14);
+//     } else if (boardReturn[14] == 2) {
+//         operator = 2;
+//         status = boardReturn.slice(0,14);
+//     } else {
+//         isEnded = true;
+//         op1Result += boardReturn[14] - 200;
+//         op2Result -= boardReturn[14] - 200;
+//     }
+// } while (!isEnded);
  
-op1Time = op1Time / 1000;
-op2Time = op2Time / 1000;
+// op1Time = op1Time / 1000;
+// op2Time = op2Time / 1000;
 
-console.log("🎉 Finished battle, result: " + op1Result + ":" + op2Result + ".");
-console.log("⏰ Processing Time: " + op1Time + ":" + op2Time + ".");
+// console.log("🎉 Finished battle, result: " + op1Result + ":" + op2Result + ".");
+// console.log("⏰ Processing Time: " + op1Time + ":" + op2Time + ".");
